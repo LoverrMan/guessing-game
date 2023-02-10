63b9764f1b067eae4313172ee7ce2d535829cc28
LoverrMan <livetskyi.oleksandr@gmail.com>
2023-02-10 12:13:32 +0100

    fix: add GuessingGame function

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,21 @@
+
 class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
-
     guess() {
-
+        return Math.ceil((this.min + this.max) / 2);
     }
 
     lower() {
-
+        return this.max = Math.ceil((this.max + this.min) / 2);
     }
 
     greater() {
-
+        return this.min = Math.ceil((this.max + this.min) / 2);
     }
 }
 
